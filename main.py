78d67f749c898e0351b68c6b590cb705509caeb0
PedroBoza <pboza@estudiante.uc.cl>
2026-03-26 22:01:47 -0300

    Todos los elif completados

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import random
 opciones = ["piedra","papel","tijera","lagarto","spock"]
-jugador = input("Eliga una de las opciones")
+jugador = input("Eliga una de las opciones: ")
 pc = random.choice(opciones)
 if jugador not in opciones:
     print("Opción inválida")
 else:
+    #aca van todos los elif
     if jugador == pc:
         print("Empate")
     elif jugador == "tijera" and pc in ["papel","lagarto"]:
         print("ganaste")
-    #aca van todos los elif
+    elif jugador == "papel" and pc in ["piedra","spock"]:
+        print("ganaste")
+    elif jugador == "piedra" and pc in ["lagarto","tijera"]:
+        print("ganaste")
+    elif jugador == "lagarto" and pc in ["spock","papel"]:
+        print("ganaste")
+    elif jugador == "spock" and pc in ["tijera","piedra"]:
+        print("ganaste")
     else:
-        print("pc eligio", pc)
+        print("PC eligio", pc)
         print("perdiste")
